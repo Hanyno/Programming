e6f51978416b247f2507a4bab175b6f6b1b2db78
hanyno <jankovarcik03@gmail.com>
2022-10-03 15:07:01 +0200

    lol

---
+++ datovetypy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    internal class app
+    {
+        static void Main(string[] args)
+        {
+            ulong[] fib = new ulong[100];
+            fib[0]=0;
+            fib[1]=1;
+            for (int i=2; i<100; i++)
+            {
+                fib[i] = fib[i-2] + fib[i-1];
+                Console.WriteLine(fib[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
