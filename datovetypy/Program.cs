0b9bbea0efd68eabb95677b67db2c4ed1050e385
hanyno <jankovarcik03@gmail.com>
2022-10-03 15:13:21 +0200

    lol

--- datovetypy/Program.cs
+++ datovetypy/Program.cs
@@ -6,14 +6,18 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            ulong[] fib = new ulong[100];
-            fib[0]=0;
-            fib[1]=1;
-            for (int i=2; i<100; i++)
-            {
-                fib[i] = fib[i-2] + fib[i-1];
-                Console.WriteLine(fib[i]);
-            }
+            //Fibonnaciho posloupnost
+
+            //ulong[] fib = new ulong[100];         //vytvoření polí s indexem 0 až 99
+            //fib[0]=0;                             //definice pole s indexem 0
+            //fib[1]=1;                             //definice pole s indexem 1
+            //for (int i=2; i<100; i++)
+            //{
+            //    fib[i] = fib[i-2] + fib[i-1];
+            //    Console.WriteLine(fib[i]);
+            //}
+            
+            
             Console.ReadLine();
         }
     }
