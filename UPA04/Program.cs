53e52707667ee7e128c16289bb02bd1ffd9c62d2
hanyno <jankovarcik03@gmail.com>
2022-10-10 17:22:50 +0200

    lol

--- UPA04/Program.cs
+++ UPA04/Program.cs
@@ -28,11 +28,43 @@ namespace Program
             }
             **/
 
+            /**
             string[] hry = new string[] {"League of Bugs", "Counter-Strike:Local Defensive", "Snowdriver", "DOOM not eternal", "The Italian plumber"};
             foreach (string polozka in hry)
             {
                 Console.WriteLine(polozka);
             }
+            **/
+            
+            string[] otazky = new string[] {"Jaké je hlavní město Madagaskaru?", "Kolik je 2+2?", "Jaká je šance, že tohle bude fungovat?"};
+            string[,] odpovedi = new string[3,2] {{"1 - Madagaskar", "2 - Antananarivo"}, {"1 - 5", "2 - 4"}, {"1 - 50%" ,"2 - 30%"}};
+            int body = 0;
+            for (int i = 0; i<=2; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(otazky[i]);
+                for (int j = 0; j<=1; j++)
+                {
+                    Console.WriteLine(odpovedi[i,j]);
+                }
+                int odpoved = int.Parse(Console.ReadLine());
+                
+                switch (i, odpoved)
+                {
+                    case (0,2):
+                        body++;
+                        break;
+                    case (1,2):
+                        body++;
+                        break;
+                    case (2,1):
+                        body++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("Máte celkem {0} bodů.", body);
             Console.ReadKey();
         }
     }
