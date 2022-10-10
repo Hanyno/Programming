24e41035e5e25aa340bfcc68c467729035552dd8
hanyno <jankovarcik03@gmail.com>
2022-10-10 14:43:52 +0200

    lol

--- UPA04/Program.cs
+++ UPA04/Program.cs
@@ -6,7 +6,25 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("Vyberte váš OS, který právě používáte:");
+            Console.WriteLine("1 - Windows \n2 - MacOS \n3 - Linux");
+            int vyber;
+            int.TryParse(Console.ReadLine(), out vyber);
+            switch (vyber)
+            {
+                case 1:
+                    Console.WriteLine("Používáte Windows.");
+                    break;
+                case 2:
+                    Console.WriteLine("Používáte MacOS.");
+                    break;
+                case 3:
+                    Console.WriteLine("Používáte Linux.");
+                    break;
+                default:
+                    Console.WriteLine("Nepoužíváte ani jeden z výše zmíněných OS nebo jste špatně zadal výběr.");
+                    break;
+            }
             Console.ReadKey();
         }
     }
