09f7a440c3ec736a55542118ca4eabd7d3d990dc
hanyno <jankovarcik03@gmail.com>
2022-10-18 19:37:01 +0200

    lol

--- UPA04/Program.cs
+++ UPA04/Program.cs
@@ -36,6 +36,8 @@ namespace Program
             }
             **/
             
+            //kvíz
+            /*
             string[] otazky = new string[] {"Jaké je hlavní město Madagaskaru?", "Kolik je 2+2?", "Jaká je šance, že tohle bude fungovat?"};
             string[,] odpovedi = new string[3,2] {{"1 - Madagaskar", "2 - Antananarivo"}, {"1 - 5", "2 - 4"}, {"1 - 50%" ,"2 - 30%"}};
             int body = 0;
@@ -66,6 +68,68 @@ namespace Program
             }
             Console.Clear();
             Console.WriteLine("Máte celkem {0} bodů.", body);
+            Console.ReadKey();
+            */
+
+            //kniha jízd
+
+            //Deklarace polí a potřebných proměnných
+            Console.WriteLine("Dobrý den. Vítejte v knize jízd.");
+            Console.WriteLine("Kolik aut chcete zapasat do knihy jízd?");
+            int.TryParse(Console.ReadLine(), out int pocetAut);
+            string[] auto = new string[pocetAut];
+            string[] palivo = new string[pocetAut];
+            double[] spotreba = new double[pocetAut];
+            double[] cenaPaliva = new double[pocetAut];
+            double[] celkovaCena = new double [pocetAut];
+            double[] celkovaVzdalenost2 = new double[pocetAut];
+            double celkovaVzalenost = 0;
+            double celkovaCenaFinal = 0;
+
+            //Vyplnění pořebných dat od uživatele pro každé auto
+            for (int i = 0; i < pocetAut; i++)
+            {
+                celkovaVzalenost = 0;
+                Console.Clear();
+                Console.WriteLine("Jaké je vaše {0}. auto?", i+1);
+                auto[i] = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Na jaké palivo {0} jezdí?", auto[i]);
+                palivo[i] = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Jaká je spotřeba auta {0} v l/100km?", auto[i]);
+                double.TryParse(Console.ReadLine(), out spotreba[i]);
+                Console.Clear();
+                Console.WriteLine("Jaká je nynější cena paliva {0}?", palivo[i]);
+                double.TryParse(Console.ReadLine(), out cenaPaliva[i]);
+                Console.Clear();
+                Console.WriteLine("Kolikrát jste s tímto autem jel tento měsíc?");
+                int.TryParse(Console.ReadLine(),out int pocetJizd);
+                double[,] najeteKilometry = new double[pocetAut,pocetJizd];
+
+                //Vyplnění vzdáleností jednotlivých cest uživatele
+                for (int j = 0; j < pocetJizd; j++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Zadejte vzdálenost {0}. cesty v km.", j+1);
+                    double.TryParse(Console.ReadLine(), out najeteKilometry[i,j]);
+                    celkovaVzalenost = celkovaVzalenost + najeteKilometry[i,j];
+                }
+                //Výpočet celkové vzdálenosti s daným autem a daná cena paliva, kteréauto projezdilo 
+                celkovaVzdalenost2[i] = celkovaVzalenost;
+                celkovaCena[i] = (celkovaVzdalenost2[i]/100) * spotreba[i] * cenaPaliva[i];
+            }
+            
+            //vypsání celkových vzdáleností jednotlivých aut a celkové útráty
+            for (int k = 0; k < pocetAut; k++)
+            {
+                Console.WriteLine("S autem {0} jste najezdil celkem {1} a stálo vás to celkem {2},- Kč.", auto[k], celkovaVzdalenost2[k], celkovaCena[k]);
+                celkovaVzalenost = celkovaVzalenost + celkovaVzdalenost2[k];
+                celkovaCenaFinal = celkovaCenaFinal + celkovaCena[k];
+            }
+
+            //vypsání celkové sumarizace všech cest
+            Console.WriteLine("Celkem jste dohromady s {0} auty(autem) najezdil celkem {1} km a stálo vás to celkem {2},- Kč", pocetAut, celkovaVzalenost, celkovaCenaFinal);
             Console.ReadKey();
         }
     }
