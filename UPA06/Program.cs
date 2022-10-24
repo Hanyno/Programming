37a030f68e5470c972be65aad914e737681fb4bf
hanyno <jankovarcik03@gmail.com>
2022-10-24 14:48:27 +0200

    lol

--- UPA06/Program.cs
+++ UPA06/Program.cs
@@ -6,7 +6,16 @@ namespace Program
     {
         static void Main(string[] args)
         {           
-            
+            Console.WriteLine("Zadejte číslo, ze kterého chcete vypočítat faktoriál: ");
+            int.TryParse(Console.ReadLine(), out int cislo);
+            int vysledek = 1;
+            while (cislo > 1)
+            {
+                vysledek = vysledek * cislo;
+                cislo--;
+            }
+            Console.WriteLine(vysledek);
+            Console.ReadKey();
         }
     }
 }
